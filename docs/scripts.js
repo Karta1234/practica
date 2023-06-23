1ab002fbe0ccdc2e7bfa29de50541aa381cf7744
Karta1234 <dunerat26@gmail.com>
2023-06-23 18:14:58 +0400

    fix

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,18 +1,4 @@
 
-// document.addEventListener('click',ev => {
-//
-//     if(ev.target.classList.contains("slider-list__img")){
-//         let mainImg = document.querySelector('.slider__img')
-//         let tmp = ev.target.attributes.src.value
-//         ev.target.setAttribute('src', mainImg.attributes.src.value)
-//         mainImg.setAttribute('src', tmp)
-//
-//     }
-//
-// })
-
-
-
 var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     navigation: {
